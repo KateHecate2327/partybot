b961dcc818efc33133d7af316992a50e1b16a57a
KateHecate2327 <75714658+KateHecate2327@users.noreply.github.com>
2022-04-19 10:29:19 +0500

    Update PartyBot(2).py

--- PartyBot(2).py
+++ PartyBot(2).py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+
+# In[102]:
 
 
 import random
@@ -9,23 +10,24 @@ import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
-# In[2]:
+# In[103]:
 
 
 BOT_CONFIG = {
     'intents': {
         'hello': {
-            'examples': ['Привет', 'Шалом','Приветики','Хеллоу'],
+            'examples': ['Привет', 'Шалом','Приветики','Хеллоу', 'Привет, друг'],
             'responses': ['Привет, человек', 'Оххайо гудзаймас!', 'Че как оно']
         },
         'bye': {
-            'examples': ['Я не прийду', 'Не смогу сегодня', 'Сегодня пас', 'Сори не смогу'],
+            'examples': ['Я не прийду', 'Не смогу сегодня', 'Сегодня пас', 'Сори не смогу','Не прийду', 'Сегодня не смогу','Без меня', 'Простите, не прийду'],
             'responses': ['Еще увидимся', 'Ты чё, пес!', 'Ну и лох', 'А жаль:(']
         },
         'go_party': {
-            'examples': ['Кто сегодня в офис', 'Сегодня туса','Сегодня хакатон', 'Заказываем', 'Заказали', 'Закажите','Натаныча','Междубулками', 'Я собираюсь выезжать', 'Выезжаю', 'Во сколько движ', 'движ', 'афтерпати','заказываем в фарфор', 'кто собирается', 'останется на афтерпати', 'придешь в офис', 'на тусич', 'можно подтягиваться', 'мы в офисе', 'возьми калик свой', 'И табак', 'угли', 'кальян', 'хакатонная туса', 'Откройте',  'подсветку взял кальянную', 'калик пакую и буду выезжат до восьми', 'должен приехать', 'приеду', 'Купи мне пива', 'Я пошёл в КБ', 'кому нибудь че нибудь надо захватить?',' Идешь сегодня?', 'курить калик', 'бухич в офисе', 'Ждем', 'в джаст дэнс', 'кто придет', 'Офис', 'потанцевать джастденс', 'тусы в офисе', 'Ещё не закончили ты придешь?', 'Сколько людей соберется?', 'Калик курить', 'Чо за движ пятничный?', 'Тусим?', 'Кому чо брать', 'Возьмите мне сидр какой-нибудь', 'вино', 'алкашка', 'Есть кто в офисе?', 'Мы в течении часа приедем', 'Во скок собраться планируете', 'еду', 'уже еду', 'тусич', 'бухич', 'Шо, кто в офисе'],
+            'examples': ['Кто сегодня в офис', 'Сегодня туса','Сегодня хакатон', 'Заказываем', 'Заказали', 'Закажите','Натаныча','Междубулками', 'Я собираюсь выезжать', 'Выезжаю', 'Во сколько движ', 'движ', 'афтерпати','заказываем в фарфор', 'кто собирается', 'останется на афтерпати', 'придешь в офис', 'на тусич', 'можно подтягиваться', 'мы в офисе', 'возьми калик свой', 'И табак', 'угли', 'кальян', 'хакатонная туса', 'Откройте',  'подсветку взял кальянную', 'калик пакую и буду выезжат до восьми', 'Купи мне пива', 'КБ', 'кому нибудь че нибудь надо захватить?',' Идешь сегодня?', 'курить калик', 'бухич в офисе', 'Ждем', 'в джаст дэнс', 'кто придет', 'Офис', 'потанцевать джастденс', 'тусы в офисе', 'Ещё не закончили ты придешь?', 'Сколько людей соберется?', 'Калик курить', 'Чо за движ пятничный?', 'Тусим?', 'Кому чо брать', 'Возьмите мне сидр какой-нибудь', 'вино', 'алкашка', 'Есть кто в офисе?', 'Мы в течении часа приедем', 'Во скок собраться планируете', 'еду', 'уже еду', 'тусич', 'бухич', 'Шо, кто в офисе'],
             'responses': ['Позвони в охрану, пёс', 'Не забудь позвонить в охрану']
         },
     },
@@ -43,13 +45,47 @@ for intent, intent_data in BOT_CONFIG['intents'].items():
         y.append(intent)
 
         
+
+
+
+# In[104]:
+
+
 vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(2,4))
 X = vectorizer.fit_transform(X_texts)
-clf = LinearSVC().fit(X,y)
+print(len(vectorizer.get_feature_names()))
+
+
+# In[105]:
+
+
+#обучение support vector classifier
+#clf = LinearSVC().fit(X,y)
+scores = []
+for i in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33)
+    clf = LinearSVC().fit(X_test, y_test)
+    score = clf.score(X_test, y_test)
+    scores.append(score)
+    
+sum(scores)/len(scores)
+
+
+# In[106]:
+
+
 clf_proba = SVC(probability = True).fit(X,y)
+clf.score(X,y)
 
 
-# In[3]:
+# In[107]:
+
+
+#тест
+clf.predict(vectorizer.transform(['мне страшно']))
+
+
+# In[108]:
 
 
 def get_intent(question):
@@ -60,27 +96,30 @@ def get_intent(question):
         for example in examples:
             dist = nltk.edit_distance(question, example)
             dist_percentage = dist / len(example)
-            if dist_percentage < 0.2:
+            if dist_percentage < 0.6:
                 return intent
-        #index = list(clf_proba.classes_).index(intent)
+        #index = list(clf_proba.classes_).index(intent)  нлтк для пиведения к инфинитиву
         #proba = clf_proba.predict_proba(question_vector)[0][index]
         #print(intent, proba)
         #if proba > 0.20: # если класс не подходит нужно не отправлять ответ, не ддосить чат
             #return intent
-get_intent('Выехал')
+get_intent('среда')
+print(intent)
 
 
-# In[4]:
+# In[117]:
 
 
-def qet_answer_by_intent(intent):
+def get_answer_by_intent(intent):
     if intent in BOT_CONFIG['intents']:
         phrases = BOT_CONFIG['intents'][intent]['responses']
-        return random.choise(phrases)
+        return random.choice(phrases)
+    
+qet_answer_by_intent('bye')
     
 
 
-# In[5]:
+# In[118]:
 
 
 #def filter_text(text):
@@ -90,7 +129,7 @@ def qet_answer_by_intent(intent):
    # return text
 
 
-# In[6]:
+# In[119]:
 
 
 def get_failure_phrase():
@@ -98,7 +137,7 @@ def get_failure_phrase():
     return random.choice(phrases)
 
 
-# In[7]:
+# In[120]:
 
 
 #def get_intent(question):
@@ -110,7 +149,7 @@ def get_failure_phrase():
    #                 return intent
 
 
-# In[8]:
+# In[121]:
 
 
 def bot(question):
@@ -122,15 +161,21 @@ def bot(question):
         if answer:
             return answer
     # Генерируем ответ по контексту из файла
-    answer = generate_answer_by_text(question)
-    if answer:
-        return answer
+    #answer = generate_answer_by_text(question)
+    #if answer:
+        #return answer
 
 #answer = get_failure_phrase()
 #return answer
 
 
-# In[9]:
+# In[122]:
+
+
+print(bot('туса'))
+
+
+# In[ ]:
 
 
 question = None
@@ -138,30 +183,6 @@ while question not in ['']:
     question = input()
     answer = bot(question)
     print(answer)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
