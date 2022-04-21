530076b82684305705b76c50b26411f62d8561fb
KateHecate2327 <75714658+KateHecate2327@users.noreply.github.com>
2022-04-21 20:14:09 +0500

    Update PartyBot.py

--- PartyBot.py
+++ PartyBot.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-# In[102]:
 
 
 import random
@@ -13,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
-# In[103]:
+# In[86]:
 
 
 BOT_CONFIG = {
@@ -23,11 +18,23 @@ BOT_CONFIG = {
             'responses': ['Привет, человек', 'Оххайо гудзаймас!', 'Че как оно']
         },
         'bye': {
-            'examples': ['Я не прийду', 'Не смогу сегодня', 'Сегодня пас', 'Сори не смогу','Не прийду', 'Сегодня не смогу','Без меня', 'Простите, не прийду'],
-            'responses': ['Еще увидимся', 'Ты чё, пес!', 'Ну и лох', 'А жаль:(']
+            'examples': ['Я не прийду', 'Не смогу сегодня', 'Сегодня пас', 'Сори не смогу','Не прийду', 'Сегодня не смогу',
+                         'Без меня', 'Простите, не прийду', 'на сегодня уже естть планы'],
+            'responses': ['Еще увидимся', 'Ты чё, пёс!', 'Ну и лох', 'А жаль:(']
         },
         'go_party': {
-            'examples': ['Кто сегодня в офис', 'Сегодня туса','Сегодня хакатон', 'Заказываем', 'Заказали', 'Закажите','Натаныча','Междубулками', 'Я собираюсь выезжать', 'Выезжаю', 'Во сколько движ', 'движ', 'афтерпати','заказываем в фарфор', 'кто собирается', 'останется на афтерпати', 'придешь в офис', 'на тусич', 'можно подтягиваться', 'мы в офисе', 'возьми калик свой', 'И табак', 'угли', 'кальян', 'хакатонная туса', 'Откройте',  'подсветку взял кальянную', 'калик пакую и буду выезжат до восьми', 'Купи мне пива', 'КБ', 'кому нибудь че нибудь надо захватить?',' Идешь сегодня?', 'курить калик', 'бухич в офисе', 'Ждем', 'в джаст дэнс', 'кто придет', 'Офис', 'потанцевать джастденс', 'тусы в офисе', 'Ещё не закончили ты придешь?', 'Сколько людей соберется?', 'Калик курить', 'Чо за движ пятничный?', 'Тусим?', 'Кому чо брать', 'Возьмите мне сидр какой-нибудь', 'вино', 'алкашка', 'Есть кто в офисе?', 'Мы в течении часа приедем', 'Во скок собраться планируете', 'еду', 'уже еду', 'тусич', 'бухич', 'Шо, кто в офисе'],
+            'examples': ['Кто сегодня в офис', 'захвати', 'Сегодня туса','афтерпарти','туса','Сегодня хакатон', 
+                         'хакатон', 'Заказываем', 'Заказали', 'Закажите','Натаныча','Междубулками', 'Я собираюсь выезжать',
+                         'Выезжаю', 'Во сколько движ', 'движ', 'афтерпати','заказываем в фарфор', 'кто собирается',
+                         'останется на афтерпати', 'придешь в офис', 'на тусич', 'можно подтягиваться', 'мы в офисе', 
+                         'возьми калик свой', 'И табак', 'угли', 'кальян', 'калик', 'хакатонная туса', 'Откройте', 
+                         'подсветку взял кальянную', 'кальян будет','калик пакую и буду выезжат до восьми', 'Купи мне пива', 'КБ',
+                         'кому нибудь че нибудь надо захватить?',' Идешь сегодня?', 'курить калик', 'бухич в офисе',
+                         'Ждем', 'в джаст дэнс', 'кто придет', 'Офис', 'потанцевать джастденс', 'тусы в офисе', 
+                         'Ещё не закончили ты придешь?', 'Сколько людей соберется?', 'Калик курить', 'Чо за движ пятничный?', 
+                         'Тусим?', 'Кому чо брать', 'Возьмите мне сидр какой-нибудь', 'вино', 'алкашка', 'Есть кто в офисе?', 
+                         'Мы в течении часа приедем', 'Во скок собраться планируете', 'еду', 'го парти', 'уже еду', 'тусич', 'бухич', 
+                         'Шо, кто в офисе', 'кому что купить?', 'захвати угли', 'калик подымим', 'хукатон', 'приветик, кальян будет?'],
             'responses': ['Позвони в охрану, пёс', 'Не забудь позвонить в охрану']
         },
     },
@@ -37,18 +44,22 @@ BOT_CONFIG = {
     ]
 }
 X_texts = [] # реплики
-y = [] # классы, если класс не подходит нужно не отправлять ответ, не ддосить чат
+y = [] # классы, если класс не подходит не нужно отправлять ответ, не ддосить чат
 
 for intent, intent_data in BOT_CONFIG['intents'].items():
     for example in intent_data['examples']:
         X_texts.append(example)
         y.append(intent)
 
-        
+
+# In[87]:
 
 
-
-# In[104]:
+#Мы не можем работать с текстом напрямую при использовании алгоритмов машинного обучения
+#нужно преобразовать текст в числа
+def filter_text(X_texts):
+    X_texts = text.lower()
+    X_texts = [c for c in text if c in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя- ']
 
 
 vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(2,4))
@@ -56,37 +67,39 @@ X = vectorizer.fit_transform(X_texts)
 print(len(vectorizer.get_feature_names()))
 
 
-# In[105]:
+# In[88]:
 
 
 #обучение support vector classifier
-#clf = LinearSVC().fit(X,y)
-scores = []
-for i in range(10):
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.33)
-    clf = LinearSVC().fit(X_test, y_test)
-    score = clf.score(X_test, y_test)
-    scores.append(score)
-    
-sum(scores)/len(scores)
+clf = LinearSVC().fit(X, y)
 
 
-# In[106]:
+# In[89]:
 
 
-clf_proba = SVC(probability = True).fit(X,y)
-clf.score(X,y)
+#clf_proba = LinearSVC.fit(X, y)
+#clf.score(X, y)
 
 
-# In[107]:
+# In[90]:
 
 
 #тест
-clf.predict(vectorizer.transform(['мне страшно']))
+clf.predict(vectorizer.transform(['го парти']))
 
 
-# In[108]:
+# In[91]:
 
+
+
+
+#def get_intent(question):
+#    for intent, intent_data in BOT_CONFIG['intents'].items():
+#        for example in intent_data['examples']:
+#            filtered_example = filter_text(example)
+#            dist = nltk.edit_distance(filtered_example, filter_text(question))
+#            if dist / len(filtered_example) < 0.4:
+#                    return intent
 
 def get_intent(question):
         question_vector = vectorizer.transform([question])
@@ -96,18 +109,14 @@ def get_intent(question):
         for example in examples:
             dist = nltk.edit_distance(question, example)
             dist_percentage = dist / len(example)
-            if dist_percentage < 0.6:
+            if dist_percentage < 0.4:
                 return intent
-        #index = list(clf_proba.classes_).index(intent)  нлтк для пиведения к инфинитиву
-        #proba = clf_proba.predict_proba(question_vector)[0][index]
-        #print(intent, proba)
-        #if proba > 0.20: # если класс не подходит нужно не отправлять ответ, не ддосить чат
-            #return intent
-get_intent('среда')
+            
+get_intent('хуйня')
 print(intent)
 
 
-# In[117]:
+# In[92]:
 
 
 def get_answer_by_intent(intent):
@@ -115,21 +124,9 @@ def get_answer_by_intent(intent):
         phrases = BOT_CONFIG['intents'][intent]['responses']
         return random.choice(phrases)
     
-qet_answer_by_intent('bye')
-    
 
 
-# In[118]:
-
-
-#def filter_text(text):
-    #text = text.lower()
-    #text = [c for c in text if c in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя- ']
-    #text = ''.join(text)
-   # return text
-
-
-# In[119]:
+# In[93]:
 
 
 def get_failure_phrase():
@@ -137,19 +134,7 @@ def get_failure_phrase():
     return random.choice(phrases)
 
 
-# In[120]:
-
-
-#def get_intent(question):
-   # for intent, intent_data in BOT_CONFIG['intents'].items():
-   #     for example in intent_data['examples']:
-   #         filtered_example = filter_text(example)
-   #         dist = nltk.edit_distance(filtered_example, filter_text(question))
-   #         if dist / len(filtered_example) < 0.4:
-   #                 return intent
-
-
-# In[121]:
+# In[94]:
 
 
 def bot(question):
@@ -160,22 +145,23 @@ def bot(question):
         answer = get_answer_by_intent(intent)
         if answer:
             return answer
-    # Генерируем ответ по контексту из файла
-    #answer = generate_answer_by_text(question)
-    #if answer:
-        #return answer
-
-#answer = get_failure_phrase()
-#return answer
+#    answer = get_failure_phrase()
+#    return answer
 
 
-# In[122]:
+# In[95]:
 
 
-print(bot('туса'))
+print(bot('приветик, кальян будет?'))
 
 
 # In[ ]:
+
+
+
+
+
+# In[57]:
 
 
 question = None
@@ -183,27 +169,6 @@ while question not in ['']:
     question = input()
     answer = bot(question)
     print(answer)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
