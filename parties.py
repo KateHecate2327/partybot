10784d1821fb91a3aabafa1f9bb080ddb74260e4
Gorill <kirill.ivanov96@gmail.com>
2022-04-22 16:32:14 +0500

    Добавил отправку сообщений

--- parties.py
+++ parties.py
@@ -11,12 +11,16 @@ class Party:
     def __init__(self, chat_id: str):
         self.day = datetime.date.today()
         self.chat_id = chat_id
+        self.participants = []
 
     def add_participant(self, participant):
         self.participants.append(participant)
     
     def mention(self):
-        return [f"@{p}" for p in self.participants]
+        mention = "Не забудьте позвонить в охране"
+        for p in self.participants:
+            mention += f", @{p}"
+        return mention
 
 
 class Parties:
@@ -25,11 +29,11 @@ class Parties:
     def __init__(self):
         self.last_party = None
 
-    def get_party(self, chat_id: str):
-        if self.last_party and self.last_party.day == datetime.date.today():
-            return self.last_party
+    def get_party(self):
+        # if self.last_party and self.last_party.day == datetime.date.today():
+        return self.last_party
         
+    def new_party(self, chat_id):
         new_party = Party(chat_id)
         self.last_party = new_party
         return new_party
-
--- tg.py
+++ tg.py
@@ -1,20 +1,29 @@
+import datetime
 import os
-from telegram import Update
+import time
+from telegram import Update, Bot
 from telegram.ext import CallbackContext, Filters, MessageHandler, Updater
+from multiprocessing import Process
+import shelve
 
 from PartyBot import PARTY_INTENT, get_answer
-from parties import Party, Parties
+from parties import Parties
 
 from dotenv import load_dotenv
 
-# Читаем конфигурацию
 load_dotenv()
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-updater = Updater( token=BOT_TOKEN, use_context=True)
+updater = Updater(token=BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 parties = Parties()
 
+try:
+    os.remove("partystorage.bak")
+    os.remove("partystorage.dat")
+    os.remove("partystorage.dir")
+except:
+    pass
 
 def echo(update: Update, context: CallbackContext):
     response = get_answer(update.message.text)
@@ -26,13 +35,50 @@ def echo(update: Update, context: CallbackContext):
     text, intent = response
     if intent != PARTY_INTENT:
         context.bot.send_message(chat_id=chat_id, text=text)
+        return
 
-    print(update.message)
+    party = parties.get_party()
+    if not party:
+        party = parties.new_party(chat_id=chat_id)
 
+    party.add_participant(update.message.from_user.username)
+
+    db = shelve.open("partystorage")
+    db["parties"] = parties
+    db.close()
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
 
-updater.start_polling()
-updater.idle()
+def start_polling():
+    updater.start_polling()
+    updater.idle()
+
+
+def start_printing():
+    while True:
+        now = datetime.datetime.utcnow()
+        ufa_21_00 = 16
+        ufa_22_00 = 17
+
+        if now.hour < 11 or now.hour > 12:
+            print("еще рано")
+            time.sleep(60*60)
+            continue
+
+        db = shelve.open("partystorage")
+        parties = db.get("parties")
+        db.close()
+        if parties:
+            party = parties.get_party()
+
+            if party:
+                Bot(BOT_TOKEN).send_message(chat_id=party.chat_id, text=party.mention())
+                time.sleep(60*60)
+                continue
+
+        time.sleep(5)
+
+p1 = Process(target=start_polling).start()
+p2 = Process(target=start_printing).start()
